8144a1fba6a1c3569f12e692e9bbb68d1d2d3b6d
nirasan <niranirao@gmail.com>
2012-10-02 13:22:29 +0900

    ログイン済みユーザーを引き回せるように

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -32,8 +32,10 @@ App::uses('Controller', 'Controller');
  * @link http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
-    
+
     public $uses = array('User');
+
+    public $user = null;
 
     public $components = array(
         'Session',
@@ -46,6 +48,7 @@ class AppController extends Controller {
     public function beforeFilter() {
         $this->Auth->allow('index', 'view');
         $user_id = $this->Auth->user('id');
-        $this->set('user', $this->User->findById($user_id));
+        $user = $this->User->findById($user_id);
+        $this->set('user', $user);
     }
 }
